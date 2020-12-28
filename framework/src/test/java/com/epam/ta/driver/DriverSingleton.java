078c523a99608faa0a772a52a32bc8bd3f8a0ff5
Ignat Fomin <ignatfomin1@gmail.com>
2020-12-28 22:09:01 +0300

    framework update for jenkins v2

--- framework/src/test/java/com/epam/ta/driver/DriverSingleton.java
+++ framework/src/test/java/com/epam/ta/driver/DriverSingleton.java
@@ -1,6 +1,7 @@
 package com.epam.ta.driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -24,7 +25,10 @@ public class DriverSingleton {
                     driver = new ChromeDriver();
                 }
             }
-            driver.manage().window().maximize();
+            //driver.manage().window().maximize();
+            driver.manage().window().setSize(new Dimension(1920,1080));
+            driver.manage().window().setSize(new Dimension(Integer.parseInt("w"),Integer.parseInt("h")));
+
         }
         return driver;
     }
